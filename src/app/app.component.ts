e79def716e6fbeb9248c94cad1a06cf72cad8ca3
bc090402389@gmail.com <bc090402389@gmail.com>
2022-05-24 22:22:41 +0500

    changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ngPrimeTest';
+  //title = 'ngPrimeTest';
+  language?:string;
+  country?:string;
+  code?:number;
+  area?:string;
+  languageList:any[]=[
+    {language:'Urdu',
+     country:'Pakistan',
+     code:'+92',
+     area:'Asia'
+    },
+  ];
+
+  addLanguage(){
+
+    this.languageList.push(
+      {Language:this.language, country:this.country, code:this.code,area:this.area}
+      );
+  }
+  delLanguage(index:number){
+
+    this.languageList.splice(index,1);
+
+  }
+  getLanguages(){
+    return this.languageList;
+  }
+
+
+
+
+
 }
